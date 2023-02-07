d51eea0134523d2de99ba097751fc8c65e018d99
Francisco Jose da Silva Sousa Junior <fco.silva.dev@gmail.com>
2023-02-07 15:33:29 +0300

    Add header, background and search button

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,8 +1,21 @@
+import { Header } from "../../components/Header/Header";
+import background from '../../assets/git_logo.svg';
+import './styles.css';
+
 
 function App() {
   return (
     <div>
-      <h1></h1>
+      <Header />
+      <div className="content">
+        <img src={background} alt="github logo on background" className="background"/>
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
