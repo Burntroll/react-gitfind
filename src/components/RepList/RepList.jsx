1d2de7ad90bd641b7a4e6bdecc21d8d97024fb95
Francisco Jose da Silva Sousa Junior <fco.silva.dev@gmail.com>
2023-02-18 23:28:38 +0300

    Add components rendering logic

--- src/components/RepList/RepList.jsx
+++ src/components/RepList/RepList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import './styles.css';
 
 const RepList = ({title, description}) => {
+
   return (
     <div className='item-list'>
       <strong>{title}</strong>
--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,11 +1,32 @@
 import { Header } from "../../components/Header/Header";
+import { useState } from "react";
 import background from "../../assets/git_logo.svg";
-import { SearchBar } from "../../components/SearchBar/SearchBar";
-import { ProfileBox } from "../../components/ProfileBox/ProfileBox";
 import { RepList } from "../../components/RepList/RepList";
 import "./styles.css";
 
 function App() {
+
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`)
+    const newUser = await userData.json();
+
+    if(newUser.name) {
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login})
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+
+  }
   return (
     <div>
       <Header />
@@ -16,24 +37,47 @@ function App() {
           className="background"
         />
         <div className="info">
-          <SearchBar />
-          <ProfileBox />
-          <hr />
+          {/* SEARCH BAR */}
+          <div className="searchBar">
+          <input 
+            name="usuario" 
+            placeholder="@username"
+            value={user}
+            onChange={event => setUser(event.target.value)}
+          />
+          <button onClick={handleGetData}>Search</button>
+          </div>
+          {/* PROFILE SECTION */}
+          {currentUser?.name ?
+            (<> 
+              <div className="profile__box">
+                <img
+                  src={currentUser.avatar_url}
+                  className="profile"
+                  alt="user's profile"
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>)
+          : null }
+          {/* REPOSITORY SECTION */}
+          {repos?.length ? (
           <div>
             <h3 className="repository">Repositories</h3>
-            <RepList 
-              title="Repository Name" 
-              description="Tudo descrito"
-            />
-            <RepList 
-              title="Repository Name" 
-              description="Tudo descrito"
-            />
-            <RepList 
-              title="Repository Name" 
-              description="Tudo descrito"
-            />
+            {repos.map((repo) => (
+              <RepList 
+                title={repo.name} 
+                description={repo.description}
+              />
+              ))}
           </div>
+          )
+          : null}
         </div>
       </div>
     </div>
